4afe427c9a741a1018eb940167c2c7249514d3f4
JiyeonKim017 <jiyeon.fisa@gmail.com>
2026-01-09 08:45:48 +0900

    [Bronze V] Title: 개수 세기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,9 +1,5 @@
 n = int(input())
 nums = list(map(int, input().split()))
 v = int(input())
-my_count = 0
 
-for i in nums:
-    if i == v:
-        my_count += 1
-print(my_count)
+print(nums.count(v))
