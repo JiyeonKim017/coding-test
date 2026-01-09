7aa5e8c8080d1b5b0bce066c0af4e3cbdb128a5d
JiyeonKim017 <jiyeon.fisa@gmail.com>
2026-01-09 16:40:02 +0900

    Update 나머지.py

--- 백준/Bronze/02w task/3052. 나머지/나머지.py
+++ 백준/Bronze/02w task/3052. 나머지/나머지.py
@@ -1,13 +1,7 @@
-remainders = set() # 중복을 제거해주는 마법의 바구니(집합) 생성
+remainders = set()
 
 for _ in range(10):
     num = int(input())
-    remainders.add(num % 42) # 리스트의 append 대신 set은 add를 써요!
+    remainders.add(num % 42)
 
-# 중복이 제거된 바구니에 숫자가 몇 개 들어있는지 출력
 print(len(remainders))
-
-## 1. 10번 입력받으며 바로 % 42를 계산해서 리스트로 만듦
-## 2. 그 리스트를 set()으로 감싸서 중복 제거
-## 3. 그 길이를 len()으로 측정
-# print(len(set(int(input()) % 42 for _ in range(10))))
